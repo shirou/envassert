a6cd9669a85398085524d08b0f3154548295d46d
hoge_suzuki <hoge_suzuki@localhost>
2015-02-19 00:03:44 +0900

    modify cron.has_entry()

--- src/envassert/cron.py
+++ src/envassert/cron.py
@@ -1,7 +1,16 @@
 from __future__ import with_statement
-from fabric.api import run, hide
+from fabric.api import run, sudo, hide
 
 
 def has_entry(user, entry):
+	return has_entry_self(entry)
+#    with hide("everything"):
+#        return run('crontab -u %s -l | grep "%s" ; true' % (user, entry))
+
+def has_entry_self(entry):
     with hide("everything"):
-        return run('crontab -u %s -l | grep "%s" ; true' % (user, entry))
+        return entry in run('crontab -l; true')
+
+def has_entry_super(user, entry):
+    with hide("everything"):
+        return entry in sudo('crontab -u %s -l; true' % user)
